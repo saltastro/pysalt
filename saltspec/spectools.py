c694fc6b065a12cdf66f248a2f6aaff2098559d6
Steven Crawford <crawford@saltpipe.cape.saao.ac.za>
2014-08-05 17:18:32 +0200

    update specnames

--- saltspec/spectools.py
+++ saltspec/spectools.py
@@ -672,19 +672,21 @@ def getslitsize(slitname, config_file=''):
    """Return the slit size for a given slit name"""
    if slitname.strip()=='PL0060N001': return 0.6
    if slitname.strip()=='PL0100N001': return 1.0
+   if slitname.strip()=='PL0120P001': return 1.2
+   if slitname.strip()=='PL0125N001': return 1.25
    if slitname.strip()=='PL0150N001': return 1.5
    if slitname.strip()=='PL0200N001': return 2.0
    if slitname.strip()=='PL0300N001': return 3.0
    if slitname.strip()=='PL0400N001': return 4.0
-   if slitname.strip()=='PL0120P001': return 1.2
+
    try:
        return int(slitname.strip())
    except:
        pass
    msg='Assuming a slit size of 1.0'
    return 1.0
-   msg='SPECTOOLS--Slitmask name not identified in config file'
-   raise SALTSpecError(msg)
+   #msg='SPECTOOLS--Slitmask name not identified in config file'
+   #raise SALTSpecError(msg)
 
 def makesection(section):
     """Convert a section that is a list of coordinates into 
