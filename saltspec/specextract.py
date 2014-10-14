7d952c300abc3db519ea0d245711609bcab3f1e4
Steve Crawford <crawfordsm@gmail.com>
2014-10-14 20:56:15 +0200

    Update specextract.py
    remove print statements

--- saltspec/specextract.py
+++ saltspec/specextract.py
@@ -119,9 +119,7 @@ def extract (hdu, ext=1, method='normal', section=[], minsize=3.0, thresh=3.0, c
 
        #convert from air to vacuum
        if convert:  
-          print warr[0]
           warr=Spectrum.air2vac(warr)
-          print warr[1]
 
   
        #set up the sections in case of findobj
